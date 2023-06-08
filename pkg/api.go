ddf133a9af8126409f6c8147b250dd113fce0906
Aman Maharjan <mhrjn.aman@gmail.com>
2023-06-08 12:39:35 +0545

    feat: Remove unnecessary path from api

--- pkg/api.go
+++ pkg/api.go
@@ -4,39 +4,17 @@ import (
 	"encoding/json"
 	"errors"
 	"github.com/NubeIO/flow-framework/module/common"
-	"github.com/NubeIO/flow-framework/plugin/nube/system/smodel" // May need to refactor as smodel is imported from plugin
 	"github.com/NubeIO/lib-schema/systemschema"
 )
 
 const (
-	schemaNetwork     = "/schema/network"
-	schemaDevice      = "/schema/device"
-	schemaPoint       = "/schema/point"
 	jsonSchemaNetwork = "/schema/json/network"
 	jsonSchemaDevice  = "/schema/json/device"
 	jsonSchemaPoint   = "/schema/json/point"
 )
 
 func (m *Module) Get(path string) ([]byte, error) {
-	if path == schemaNetwork {
-		fns, err := m.grpcMarshaller.GetFlowNetworks("")
-		if err != nil {
-			return nil, err
-		}
-
-		fnsNames := make([]string, 0)
-		for _, fn := range fns {
-			fnsNames = append(fnsNames, fn.Name)
-		}
-
-		networkSchema := smodel.GetNetworkSchema() // Not sure if func GetNetworkSchema of smodel can be used
-		networkSchema.AutoMappingFlowNetworkName.Options = fnsNames
-		return json.Marshal(networkSchema)
-	} else if path == schemaDevice {
-		return json.Marshal(smodel.GetDeviceSchema())
-	} else if path == schemaPoint {
-		return json.Marshal(smodel.GetPointSchema())
-	} else if path == jsonSchemaNetwork {
+	if path == jsonSchemaNetwork {
 		fns, err := m.grpcMarshaller.GetFlowNetworks("")
 		if err != nil {
 			return nil, err
