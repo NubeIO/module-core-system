2612f2d0cb2afb78c0fb7c70c3aba34b7c113e13
Aman Maharjan <mhrjn.aman@gmail.com>
2023-06-10 21:39:40 +0545

    feat: Update config to comply with new function signature

--- pkg/config.go
+++ pkg/config.go
@@ -1,8 +1,15 @@
 package pkg
 
+import (
+	"github.com/go-yaml/yaml"
+	log "github.com/sirupsen/logrus"
+	"time"
+)
+
 type Config struct {
-	Schedule Schedule `yaml:"schedule"`
-	LogLevel string   `yaml:"log_level"`
+	Schedule        Schedule      `yaml:"schedule"`
+	LogLevel        string        `yaml:"log_level"`
+	ReIterationTime time.Duration `yaml:"re_iteration_time"`
 }
 
 type Schedule struct {
@@ -24,9 +31,19 @@ func (m *Module) GetConfig() interface{} {
 	return m.config
 }
 
-// ValidateAndSetConfig implements plugin.Configurer
-func (m *Module) ValidateAndSetConfig(config interface{}) error {
-	newConfig := config.(*Config)
+func (m *Module) ValidateAndSetConfig(config []byte) ([]byte, error) {
+	newConfig := &Config{}
+	if err := yaml.Unmarshal(config, newConfig); err != nil {
+		return nil, err
+	}
+	if newConfig.ReIterationTime == 0 {
+		newConfig.ReIterationTime = time.Duration(5) * time.Second
+	}
+	newConfValid, err := yaml.Marshal(newConfig)
+	if err != nil {
+		return nil, err
+	}
 	m.config = newConfig
-	return nil
+	log.Info("config is set")
+	return newConfValid, nil
 }
